fa6b6356cc19d61f3afe9ba325bac6f5aa9f3f66
Bagomot <perezzloy@gmail.com>
2021-10-20 12:45:48 +0300

    replacement tslint to eslint, refactoring code

--- nodejs-ts-project-init.sh
+++ nodejs-ts-project-init.sh
@@ -1,26 +1,44 @@
-# Скрипт автоматической инициализации новго проекта nodejs ts
- 
-echo Введите название корневой директории проекта:
- 
-read project
+#!/bin/bash
 
-mkdir $project
-cd $project
+# Run the script from the directory in which you want to create a directory with the project. 
+# Something like: sh /path/to/script
 
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+GRAY="\e[90m"
+ENDCOLOR="\e[0m"
+
+echo -e "Enter the name of the project (e.g. ${GRAY}example123${ENDCOLOR}):"
+read project_name
+
+# check exist project path
+if [ -d $project_name ]; then
+    echo -e "${RED}Project ${project_name} already exists in the current directory.${ENDCOLOR}"
+    exit 1
+fi
+
+mkdir ${project_name}
+cd ${project_name}
+
+echo -e "\n${YELLOW}Initializing git repository...${ENDCOLOR}"
 git init
 
+echo -e "\n${YELLOW}Initializing nodejs...${ENDCOLOR}"
 npm init -y
 
+echo -e "\n${YELLOW}Installing typescript and development packages...${ENDCOLOR}"
 npm install -D typescript
-npm install -D tslint
 npm install -D ts-node-dev
+npm install -D eslint
 
+echo -e "\n${YELLOW}Initializing typescript...${ENDCOLOR}"
 tsc --init
 
-./node_modules/.bin/tslint --init
+echo -e "\n${YELLOW}Initializing eslint...${ENDCOLOR}"
+./node_modules/.bin/eslint --init
 
-mkdir src
+echo -e "\n${YELLOW}Creating source files...${ENDCOLOR}"
+mkdir src && touch src/app.ts
 
-touch src/app.ts
-
-echo Инициализация проекта $project завершена.
+echo -e "\n${GREEN}COMPLETED SUCCESSFULLY!${ENDCOLOR}"
